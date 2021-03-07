018469c6f9993cd288962823310b9b5331b7ae58
msewebchen <44235578+msewebchen@users.noreply.github.com>
2021-03-07 14:46:03 +0800

    Add files via upload

--- jd_jdzz.js
+++ jd_jdzz.js
@@ -46,7 +46,7 @@ const inviteCodes = [
 !(async () => {
   $.tuanList = []
   await requireConfig();
-  if (helpAuthor) await getAuthorShareCode('https://gitee.com/jk9527/updateTeam/raw/my_master/jd_zz.json');
+  if (helpAuthor) await getAuthorShareCode('http://adguard.mseweb.tk/shareCodes/jd_zz.json');
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
